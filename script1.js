facf0b0e129f79446c4c219472e3366a36812572
Vitalii-Khmaryk <121098151+Vitalii-Khmaryk@users.noreply.github.com>
2023-01-13 20:24:27 +0200

    lesson JS#1

---
+++ script1.js
@@ -0,0 +1,17 @@
+let city = "Kyiv";
+city = "Lviv";
+let address = city;
+alert(address);
+
+let n = 2;
+let result = n * n * n;
+console.log(result);
+
+let penK = 4;
+let penPrice = 5.25;
+let pencilsK = 6;
+let pencilsPrice = 3.5;
+let pen = penK * penPrice;
+let p = pencilsK * pencilsPrice;
+let theTotalCost = pen + p;
+document.write(`Загальна вартість = ${theTotalCost}`);
